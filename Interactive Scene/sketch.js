210d1882ae588924b424acfdd83628dddcac82d1
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-09-25 09:22:48 -0600

    Added Interactive Scene

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,17 @@
+// Interactive Scene
+// Rayyaan 
+// 9/25/2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  circle(mouseX, mouseY, 50);
+}
