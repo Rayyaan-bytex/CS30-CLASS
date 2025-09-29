74380a3a37edd42b64624796f0409b80d09cfcde
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-09-29 09:12:46 -0600

    Added Interactive Assignment

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,17 +1,41 @@
 // Interactive Scene
-// Rayyaan 
+// Rayyaan Chaghtai
 // 9/25/2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let rows;
+let cols;  
+let cellSize = 50;
+let board = [];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  cols = width/cellSize;
+  rows = height/cellSize; 
+  for (let x = 0; x < cols; x++) {
+    board[x] = [];
+    for (let y=0; y < rows; y++) {
+      board[x][y] = 0;  
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  snakeGrid();
+}
 
-  circle(mouseX, mouseY, 50);
+function snakeGrid() {
+  // noStroke();
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++){
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function showSnake() {
+  
 }
