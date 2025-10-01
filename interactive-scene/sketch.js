0e48cea65e3d57013042c0bf43a03a545b6daecc
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-10-01 09:53:30 -0600

    Added some more functions

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,46 +6,30 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let rows;
-let cols;  
-let cellSize = 50;
-let board = [];
+let cellSize = 25;
+let gridSize = 12;
+let gameState = "start";
+let gameState2 = ""
 
 function setup() {
   createCanvas(600, 600);
-  cols = width/cellSize;
-  rows = height/cellSize; 
-  for (let x = 0; x < cols; x++) {
-    board[x] = [];
-    for (let y=0; y < rows; y++) {
-      board[x][y] = 0;  
-    }
-  }
 }
 
 function draw() {
-  snakeGrid();
-  food();
-}
+  background(220);
 
-function snakeGrid() {
-  // noStroke();
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++){
-      rect(x*cellSize, y*cellSize, cellSize, cellSize);
-    }
+  if (gameState === "start") {
+    snakeGrid();
+    fill(0);
+    textSize(20);
+    textAlign(CENTER);
+    text("Click To Start Snake Game", width/2, height/2);
   }
 }
 
-function showSnake() {
-  // rect()
-}
 
-function food() {
-  let newX;
-  let newY;
-  let eaten = true;
-
-  newX = floor(random(0, width/cellSize)) * cellSize;
-   
+function snakeGrid() {
+  noStroke();
+  noFill();
+  rect(50, 50, gridSize * cellSize, gridSize * cellSize);
 }
