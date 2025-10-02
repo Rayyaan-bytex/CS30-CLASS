2c78283ebc43d294667897f6868c5322e4e96b43
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-10-02 09:53:52 -0600

    Added Snake body, food as well

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -8,8 +8,16 @@
 
 let cellSize = 25;
 let gridSize = 12;
-let gameState = "start";
-let gameState2 = ""
+let gameState = "START";
+let speed = 5;
+let score = 0;
+let counter = 0;
+let snakeX = [];
+let snakeY = [];
+let snakeLength = 1;
+let foodX, foodY;
+let direction = "RIGHT";
+let nextDirection = "RIGHT";
 
 function setup() {
   createCanvas(600, 600);
@@ -18,12 +26,24 @@ function setup() {
 function draw() {
   background(220);
 
-  if (gameState === "start") {
+  if (gameState === "START") {
+    snakeGrid();
+    // fill("white");
+    // rect(50 , 75, width/2, height/2);
+    fill(0);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("Click To Start Snake Game", width / 2, height / 2);
+  }
+  else if (gameState === "PLAYING") {
+    snakeGrid();
+  }
+  else if (gameState === "GAME OVER") {
     snakeGrid();
     fill(0);
     textSize(20);
-    textAlign(CENTER);
-    text("Click To Start Snake Game", width/2, height/2);
+    textAlign(CENTER, CENTER);
+    text("GAME OVER! Click to Restart", width / 2, height / 2);
   }
 }
 
@@ -32,4 +52,39 @@ function snakeGrid() {
   noStroke();
   noFill();
   rect(50, 50, gridSize * cellSize, gridSize * cellSize);
+}
+
+fill(0);
+textSize(14);
+textAlign(LEFT, TOP);
+text("Speed: ", + speed, 50, 20);
+text("Score: ", + score, 150, 20);
+
+function playGame() {
+  counter++;
+  if (counter >= max(1, 30 - speed)) {
+    counter = 0;
+    moveSnake();
+  }
+
+  fill(255, 0, 0);
+  rect(50 + foodX * cellSize, 50 + foodY * cellSize, cellSize, cellSize);
+
+  fill(0, 255, 0);
+  for (let i = 0; i < snakeLength; i++) {
+    rect(50 + snakeX[i] * cellSize, 50 + snakeY[i] * cellSize, cellSize, cellSize);
+  }
+}
+
+function placeFood() {
+  foodX = floor(random(gridSize));
+  foodY = floor(random(gridSize));
+}
+
+function moveSnake() {
+  direction = nextDirection;
+  for (let i = snakeLength - 1; i > 0; i--) {
+    snakeX[i] = snakeX[i - 1];
+    snakeY[i] = snakeY[i - 1];
+  }
 }
