a95f97db711ebe21379b77b2219092c05aaae15b
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-10-10 09:11:56 -0600

    Made New Assignment Folder
    I made a new folder for the arrays assignment and added my basic info in the comments (of the code).

---
+++ arrays-objects/sketch.js
@@ -0,0 +1,15 @@
+// Arrays and Objects - Water/Ball Sorting Game
+// Rayyaan Chaghtai
+// Oct 10, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
