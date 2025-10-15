60bb3c6cd164cdd750d7f0ba00922a628eb36a18
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-10-15 09:53:11 -0600

    Added New Function
    Added a create button function to start my game

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gameState = "START";
+let tubes;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,8 +18,21 @@ function draw() {
 }
 
 function startScreen() {
-  fill("black");
-  textSize(50);
-  textAlign(CENTER, CENTER);
-  text("BALL SORT GAME", width/2, height/2);
-}
+  startButton = createButton("START GAME");
+  startButton.style("background-color", "green");
+  startButton.style("color", "white");
+  startButton.style("font-size", "16px");
+  startButton.style("border-radius", "10px");
+  startButton.style("padding", "10px 20px");  
+  startButton.style("cursor", "pointer");
+  startButton.position(width / 2 - 60, height / 2 + 100);
+} 
+
+// function game() {
+//   for (let i = 0; i < tubes; i++) {
+//     tubes.push(
+//       x: 100,
+//       y: ,
+//     )
+//   }
+// }
