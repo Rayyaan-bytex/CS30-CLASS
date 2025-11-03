336c32373708114471f38d6162ce0c1b90306762
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-11-03 09:53:49 -0600

    Started Working on Start Screen
    I am halfway through that and will move forward tomorrow.

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -7,9 +7,23 @@
 
 
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(500, 500 );
 }
 
 function draw() {
-  background(220);
-}
+  background("#0049B7");
+  textAlign(CENTER, CENTER);
+  fill(0);
+  textSize(36);
+  textStyle(BOLD);
+  text("🟥Tetris Game🟥", width / 2, height / 2 - 120);
+
+  textSize(25);
+  text("⬇️Click The Button Below To Start⬇️", width / 2, height / 2 - 60);
+
+  textSize(25);
+  text("INSTRUCTIONS", width / 2, height / 2 - 10);
+
+  textSize(16);
+  text("• Rotate Falling Shapes To Fit Them Into A Grid\n• Prevent The Stack From The Reaching The Top\n• Complete Solid Horizontal Lines To Score!", width / 2, height / 2 + 45);
+} 
