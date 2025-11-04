334ffb366281695b85e3b331f38ab4a2d5db6296
Rayyaan-bytex <rayyaanchaghtai@gmail.com>
2025-11-04 09:53:16 -0600

    Working on Game states
    Completed the starting page but facing some errors. My goal is to fix them and start with the main gameplay.

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -6,12 +6,51 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let gameState = "START";
+
+
 function setup() {
   createCanvas(500, 500 );
+  createStartButton();
 }
 
+
+function createStartButton() {
+  startButton = createButton("START GAME");
+  startButton.style("background-color", "cyan");
+  startButton.style("color", "black");
+  startButton.style("font-size", "16px");
+  startButton.style("font-weight", "bold");
+  startButton.style("border-radius", "10px");
+  startButton.style("padding", "10px 20px");
+  startButton.style("cursor", "pointer");
+  startButton.position(width / 2 - 72, height / 2 + 100);
+  startButton.mousePressed(startGame);
+}
+
+
+function startGame() {
+  startButton.hide();
+  drawGame();
+}
+
+
 function draw() {
-  background("#0049B7");
+  background("#60eb83");
+  if (gameState === "Start") {
+    drawStartScreen();
+  }
+  else if (gameState === "Play") {
+    background(220);
+    drawGame();   
+  }
+  else if (gameState === "Win") {
+    
+  }
+} 
+
+
+function drawStartScreen() {
   textAlign(CENTER, CENTER);
   fill(0);
   textSize(36);
@@ -26,4 +65,9 @@ function draw() {
 
   textSize(16);
   text("• Rotate Falling Shapes To Fit Them Into A Grid\n• Prevent The Stack From The Reaching The Top\n• Complete Solid Horizontal Lines To Score!", width / 2, height / 2 + 45);
-} 
+}
+
+
+function drawGame() {
+  resizeCanvas(450, windowHeight);
+}
